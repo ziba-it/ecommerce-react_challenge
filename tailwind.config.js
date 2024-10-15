1763aaa9a1c47d4f3ebd36a540175172ed36b117
Zibait-it <lucas.zibaitis@lightit.io>
2024-10-15 12:08:29 -0300

    chore: setup project

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = {
+  primary: {
+    50: "#FCFCFC",
+    100: "#E7E8EB",
+    300: "#ABAEB7",
+    400: "#90949F",
+    500: "#757A88",
+    600: "#5A5F70",
+    700: "#3F4559",
+    800: "#353A4B",
+    900: "#1C202E",
+    1000: "#2C2C2C",
+  },
+  complementary: {
+    600: "#EA580C",
+  },
+};
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+        ubuntu: ["Ubuntu", "sans-serif"],
+      },
+      colors: {
+        primary: colors.primary,
+        complementary: colors.complementary,
+      },
+    },
+  },
+  plugins: [],
+};
